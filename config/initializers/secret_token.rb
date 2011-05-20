92cc88b42745e830592855857773b42f6ca25802
Miles Tinsley <milestinsley@me.com>
2011-05-20 09:42:52 +0100

    Initial import.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ClaimAbleChallenge::Application.config.secret_token = '3a0f0b9c703bd23050a1c46cec40af1fc518369964a35586e6735435f3203a48227c2bd52647b8b61a54cf02dc30d9feda85e8aa78dfb2349e3d539c0ee3ce68'
