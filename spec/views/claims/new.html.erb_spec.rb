f7537b0147917d9248555e71dd2a5144685971e6
reborg <reborg@reborg.net>
2011-05-22 13:51:13 +0100

    fixed failing specs

--- spec/views/claims/new.html.erb_spec.rb
+++ spec/views/claims/new.html.erb_spec.rb
@@ -6,10 +6,6 @@ describe "claims/new.html.erb" do
       :claim_type => "",
       :active => "",
       :claim_location_postcode => "",
-      :customer_name => "",
-      :customer_postcode => "",
-      :customer_phone => "",
-      :customer_dob => "",
       :cost => "",
       :date_of_loss => ""
     ).as_new_record)
@@ -23,10 +19,6 @@ describe "claims/new.html.erb" do
       assert_select "input#claim_claim_type", :name => "claim[claim_type]"
       assert_select "input#claim_active", :name => "claim[active]"
       assert_select "input#claim_claim_location_postcode", :name => "claim[claim_location_postcode]"
-      assert_select "input#claim_customer_name", :name => "claim[customer_name]"
-      assert_select "input#claim_customer_postcode", :name => "claim[customer_postcode]"
-      assert_select "input#claim_customer_phone", :name => "claim[customer_phone]"
-      assert_select "input#claim_customer_dob", :name => "claim[customer_dob]"
       assert_select "input#claim_cost", :name => "claim[cost]"
       assert_select "input#claim_date_of_loss", :name => "claim[date_of_loss]"
     end
